0aaa30a7e9e45a49bbf20191c419e8ef0005306d
SveylanaBelogrudova <zlato5556@gmail.com>
2020-10-01 00:28:01 +0400

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--prev",
+    prevEl: ".hotel-slider__button--next",
+  },
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--prev",
+    prevEl: ".reviews-slider__button--next",
   },
 });
