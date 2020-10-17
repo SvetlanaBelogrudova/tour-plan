8ad7c30fd6228cf8713d63e85609e7f66b5aa069
SveylanaBelogrudova <zlato5556@gmail.com>
2020-10-17 21:22:14 +0400

    added validation

--- js/main.js
+++ js/main.js
@@ -37,14 +37,32 @@ $(document).ready(function () {
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass("modal__overlay_visible")
-    modalDialog.addClass("modal__dialog_visible")
+    modalOverlay.addClass("modal__overlay_visible");
+    modalDialog.addClass("modal__dialog_visible");
   }
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay_visible")
-    modalDialog.removeClass("modal__dialog_visible")
+    modalOverlay.removeClass("modal__overlay_visible");
+    modalDialog.removeClass("modal__dialog_visible");
   }
+  // Обработка форм
+  $(".form").each(function () { 
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required : "Write your name",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Write your phone",
+        },
+      },
+    });
+  });
 });
